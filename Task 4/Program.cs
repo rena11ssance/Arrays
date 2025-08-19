92a6e57ee97b8b0edee01e7436bc56d217e63795
lxst <lxst@YEGOR>
2025-08-19 20:25:32 +0500

    Реализованы условия 4 задания.

--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -10,6 +10,29 @@ namespace Task_4
     {
         static void Main(string[] args)
         {
+            const int sizeArray = 20;
+            int oddElement = 0;
+            Random random = new Random();
+            int[] arrayInitialization = new int[sizeArray];
+
+            Console.Write("Сгенерированные 20 чисел для массива: ");
+            for (int i = 0; i < sizeArray; i++)
+            {
+                int randomNumber = random.Next(-50, 51); // включая в рандом верхнюю границу по заданию, то есть 50.
+                arrayInitialization[i] = randomNumber;
+                Console.Write(arrayInitialization[i] + " ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < sizeArray; i++)
+            {
+                if (i % 2 == 0 && arrayInitialization[i] % 2 != 0 && arrayInitialization[i] > 0)
+                    {
+                        oddElement++;
+                    }
+            }
+            Console.Write("Количество нечетных положительных элементов, стоящих на четных местах: {0}.", oddElement);
+            Console.ReadKey();
         }
     }
 }
