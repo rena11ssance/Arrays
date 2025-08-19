bda57277ee8fc1cd0df016a521518583e336995b
lxst <lxst@YEGOR>
2025-08-19 20:03:37 +0500

    Реализовал условия 3 задачи.

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,52 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
-            Console.Write("");
+            const int sizeArray = 10;
+            Random random = new Random();
+            int[] arrayInitialization = new int[sizeArray];
+
+            Console.Write("Сгенерированные 10 чисел для массива: ");
+            for (int i = 0; i < sizeArray; i++)
+            {
+                int randomNumber = random.Next(-50, 51); // включая в рандом верхнюю границу по заданию, то есть 50.
+                arrayInitialization[i] = randomNumber;
+                Console.Write(arrayInitialization[i] + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Результат (первые пять по возрастанию, последние пять по убыванию): ");
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = i + 1; j < 5; j++)
+                {
+                    if (arrayInitialization[i] > arrayInitialization[j])
+                    {
+                        int temporaryValue = arrayInitialization[i];
+                        arrayInitialization[i] = arrayInitialization[j];
+                        arrayInitialization[j] = temporaryValue;
+                    }
+                }
+            }
+
+            for (int i = 5; i < sizeArray; i++)
+            {
+                for (int j = i + 1; j < sizeArray; j++)
+                {
+                    if (arrayInitialization[i] < arrayInitialization[j])
+                    {
+                        int temporaryValue = arrayInitialization[i];
+                        arrayInitialization[i] = arrayInitialization[j];
+                        arrayInitialization[j] = temporaryValue;
+                    }
+                }
+            }
+
+            for (int i = 0; i < sizeArray; i++)
+            {
+                Console.Write("{0} ", arrayInitialization[i] );
+            }
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
