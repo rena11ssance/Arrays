ec180e9ac2733a853d8be800b3e7e0aa20a68b66
lxst <lxst@YEGOR>
2025-08-19 21:06:57 +0500

    Реализованы условия задачи 5.

--- Task 5/Program.cs
+++ Task 5/Program.cs
@@ -10,6 +10,20 @@ namespace Task_5
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите размерность: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int [n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = (((i + j) % 2 == 0) ? 1 : 0);
+                    Console.Write("{0} ", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
