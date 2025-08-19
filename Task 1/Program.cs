086052171dd360dbca77265fbe769ba52e249755
she <she@A-RTMC-ANALIT>
2025-08-19 16:16:43 +0500

    Добавьте файлы проекта.

---
+++ Task 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int sizeArray = 7;
+            int sumValue = 0;
+            int[] arrayInitialization = new int[sizeArray];
+
+            for (int i = 0; i < sizeArray; i++)
+            {
+                Console.Write($"Введите значение: ");
+                arrayInitialization[i] = Convert.ToInt32(Console.ReadLine());
+                sumValue += arrayInitialization[i];
+            }
+
+            Console.Write("Ваш массив из 7 индексов выглядит так: ");
+
+            foreach (int num in arrayInitialization)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+
+            double average = (double)sumValue / sizeArray;
+            Console.Write("А его среднее арифметическое элементов равно: {0:F2}.", average);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int sizeArray = 15;
+            Random random = new Random();
+            int[] arrayInitialization = new int[sizeArray];
+
+            for (int i = 0; i < sizeArray; i++)
+            {
+                int randomNumber = random.Next(0, 51); // включая в рандом верхнюю границу по заданию, то есть 50.
+                arrayInitialization[i] = randomNumber;
+                Console.Write(arrayInitialization[i] + " ");
+            }
+            Console.WriteLine();
+
+            int minValue = arrayInitialization[0];
+            int maxValue = arrayInitialization[0];
+
+            foreach (int temporaryValue in arrayInitialization)
+            {
+                if (temporaryValue < minValue)
+                    minValue = temporaryValue;
+
+                else if (temporaryValue > maxValue)
+                    maxValue = temporaryValue;
+            }
+
+            Console.WriteLine("Минимальное число: {0}, максимальное {1}.", minValue, maxValue);
+
+            int sum = minValue + maxValue;
+            Console.Write("А сумма минимального и максимального чисел равняется: {0}.", sum);
+
+            Console.ReadKey();
+        }
+    }
+}
